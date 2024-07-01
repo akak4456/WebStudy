2c3cb2c8f2fb49ae01a1c89ebe918fbdff3a8a03
akak4456 <akak4456@naver.com>
2024-07-01 17:48:15 +0900

    [ADD] JDBC 3일차 - PreparedStatement 회원관리 프로그램
    1. 입력값 검증을 통한 SQLi를 방어할 수 있는 PreparedStatement
    2. 객체 생성시 쿼리문을 미리 컴파일하는 과정을 거침
    3. 위치홀더를 통해서 입력값이 필요한 곳을 표시
    4. 쿼리문을 실행하기 전 setString(), setInt() 등의 메소드로 값을 넣어줌
    5. 쿼리문을 실행하는 메소드 executeQuery(), executeUpdate() 에는 전달값이 필요없음

---
+++ AdeleJDBC/src/com/kh/jdbc/day03/pstmt_member/run/MemberRun.java
@@ -0,0 +1,10 @@
+package com.kh.jdbc.day03.pstmt_member.run;
+
+import com.kh.jdbc.day03.pstmt_member.view.MemberView;
+
+public class MemberRun {
+	public static void main(String[] args) {
+		MemberView view = new MemberView();
+		view.startProgram();
+	}
+}
