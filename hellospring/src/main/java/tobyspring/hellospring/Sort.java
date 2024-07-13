c9c5f17a9b7bdc8abc25afc8b6f606829f7d46b2
akak4456 <akak4456@naver.com>
2024-07-13 21:31:08 +0900

    [ADD] Sort 에 쓰인 전략패턴 구현함

---
+++ hellospring/src/main/java/tobyspring/hellospring/Sort.java
@@ -0,0 +1,15 @@
+package tobyspring.hellospring;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<String> scores = Arrays.asList("z", "x", "spring", "java");
+        Collections.sort(scores, Comparator.comparingInt(String::length).reversed());
+
+        scores.forEach(System.out::println);
+    }
+}
