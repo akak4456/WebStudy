8de666d3a66b20f684d3b920429b225a91c0c042
akak4456 <akak4456@naver.com>
2024-07-18 07:36:33 +0900

    [CNG] 테스트를 위해 코드 단순화함

--- hellospring/src/main/java/tobyspring/hellospring/Client.java
+++ hellospring/src/main/java/tobyspring/hellospring/Client.java
@@ -15,15 +15,6 @@ public class Client {
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment1: " + payment1);
-        System.out.println("-----------------------------------------------\n");
-        TimeUnit.SECONDS.sleep(1);
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment2);
-
-        System.out.println("-----------------------------------------------\n");
-        TimeUnit.SECONDS.sleep(2);
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment3);
+        System.out.println("Payment: " + payment1);
     }
 }
