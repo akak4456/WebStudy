4b3b9723013b6eba58488ffff1029dd778ff10d0
akak4456 <akak4456@naver.com>
2024-08-03 18:24:31 +0900

    [ADD] Order 에 서비스를 적용함
    1. 서비스를 적용해서 특정 기술에 의존하지 않도록 함
    2. 트랜잭션 매니저 같이 서비스에 필요하지만 서비스와 관점이 다른 것도 추가를 해 봄

--- hellospring/src/main/java/tobyspring/hellospring/order/Order.java
+++ hellospring/src/main/java/tobyspring/hellospring/order/Order.java
@@ -4,13 +4,9 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
@@ -43,5 +39,17 @@ public class Order {
 
     public BigDecimal getTotal() {
         return total;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
     }
 }
