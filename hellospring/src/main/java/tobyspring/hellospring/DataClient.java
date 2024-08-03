290aff4557250559d2bbfe056082529f3011e4ac
akak4456 <akak4456@naver.com>
2024-08-03 14:47:43 +0900

    [ADD] OrderRepository 구현함. 스프링 예외 처리 이용해봄

--- hellospring/src/main/java/tobyspring/hellospring/DataClient.java
+++ hellospring/src/main/java/tobyspring/hellospring/DataClient.java
@@ -2,8 +2,15 @@ package tobyspring.hellospring;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,16 +18,21 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
+        try {
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
+                System.out.println(order);
 
-        System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
+                return null;
+            });
+        } catch(DataIntegrityViolationException e) {
+            System.out.println("주문번호 중복 복구 작업");
+        }
     }
 }
